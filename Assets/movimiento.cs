5cf1479ad0de1a8f7a472cd1f309256839bbc897
GelenSastoque <est.gelen.sastoque@unimilitar.edu.co>
2022-05-08 20:39:55 -0500

    waves

--- Assets/movimiento.cs
+++ Assets/movimiento.cs
@@ -6,11 +6,12 @@ public class movimiento : MonoBehaviour
 {
     //private new Rigidbody rigidbody;
     public Transform barquito;
-    public float velocidad = 0.0f, x = 0.0f, z = 0.0f, h = 0.1f;
+    public float velocidad = 0.0f, x = 0.0f, z = 0.0f, h = 0.1f,y=0.0f;
     private float velx = 0.5f, velz = 0.0f, dt = 0.1f;
     private float tx = 0.0f, tz = 0.0f, acelaz = 0.0f, acelax = 0.0f, angvx = 0.0f, angvz = 0.0f, inercia = 50f, dw = 0.0f, acela = 0.0f, angvelo = 0.0f, dtheta = 0.0f, theta = 0.0f;
     //public GameObject esferita;
-    private bool prueba;
+    private bool prueba,pruebasopotosientasmil=false;
+    
     private Vector3 Fuerza;
     private Vector3 PosicionBarco;
     private Vector3 velocidadBarco;
@@ -18,52 +19,29 @@ public class movimiento : MonoBehaviour
     private Vector3 aceleracionC;
     private Vector3 lookAtanclaje;
     private Vector3 aceleracionAngu;
+    float [] Posiciones = new float[4];
     //public float aumentacelec = 2.0f;
-    private float anclajeX = 0.0f, anclajeZ = 0.0f, anclajeMagnitud = 0.0f;
+    private float aumento = 0.2f;
+    private float aumentotheta = 0.1f;
+    private bool prueba3 = true;
+    private bool prueba2 = true;
+    private bool prueba4 = true;
+    private float anclajeX = 0.0f, anclajeZ = 0.0f, anclajeMagnitud = 0.0f,alpha=0.0f;
+    private int cont=0;
 
     // Start is called before the first frame update
     void Start()
     {
-        PosicionBarco = new Vector3(x, 0.0f, z);
-        barquito.transform.position = new Vector3(x, 0f, z);
+        PosicionBarco = new Vector3(x, y, z);
+        barquito.transform.position = new Vector3(x, y, z);
     }
 
-    float movimientoobj(float posicion, float prueba, float vel)
-    {
-        posicion = posicion + (h * vel * prueba);
-        return posicion;
-    }
-    float velocidadobj(float fuerza, float velo)
-    {
-
-        velo += fuerza * h;
-        return velo;
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
-        //if (x > 33f)
-        //{
-        //    x = -33f;
-        //}
-        //if (x < -33f)
-        //{
-        //    x = 33f;
-        //}
-        //if (z < -58f)
-        //{
-        //    z = 14f;
-        //}
-        //if (z > 14f)
-        //{
-        //    z = -58f;
-        //}
-        //calculos(true);
-        //if (velx<0.0f)
-        //{
-        //    velx = 0.2f;
-        //}
+        int acum = 0;
+        movolas();
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             //aumentacelec -= 0.5f;
@@ -96,6 +74,7 @@ public class movimiento : MonoBehaviour
         {
             actualizarEuler();
             calculos();
+           
             //esferita.transform.position = PosicionAnclaje;
             //barquito.transform.position = new Vector3(x, 0f, z);
         }
@@ -103,7 +82,9 @@ public class movimiento : MonoBehaviour
         {
             moverderecho();
         }
-        barquito.transform.position = new Vector3(x, 0f, z);
+        
+        barquito.transform.position = new Vector3(x, y, z);
+        
         barquito.transform.LookAt(PosicionBarco - velocidadBarco);
 
     }
@@ -122,7 +103,7 @@ public class movimiento : MonoBehaviour
     void calculos()
     {
         Fuerza = new Vector3(0.0f, 0.0f, 0.0f);
-        PosicionBarco = new Vector3(x, 0.0f, z);
+        PosicionBarco = new Vector3(x, y, z);
         //PosicionAnclaje= new Vector3(-9.0f,0.0f,anclajeZ);
         velocidadBarco = new Vector3(velx, 0.0f, velz);
         aceleracionC = new Vector3(0.0f, 0.0f, 0.0f);
@@ -132,7 +113,7 @@ public class movimiento : MonoBehaviour
     }
     void veclook(bool direccion)
     {
-        PosicionBarco = new Vector3(x, 0.0f, z);
+        PosicionBarco = new Vector3(x,y, z);
         anclajeMagnitud = -10.0f;
         float lookAtanclajeX, lookAtanclajeZ;
         if (direccion == true)
@@ -145,8 +126,98 @@ public class movimiento : MonoBehaviour
             lookAtanclajeX = velocidadBarco.z;
             lookAtanclajeZ = -velocidadBarco.x;
         }
-        Vector3 lookAtanclaje = new Vector3(lookAtanclajeX, 0.0f, lookAtanclajeZ).normalized * anclajeMagnitud;
+        Vector3 lookAtanclaje = new Vector3(lookAtanclajeX,0f, lookAtanclajeZ).normalized * anclajeMagnitud;
         //print("magnitud lookAt: "+ lookAtanclaje.magnitude);
         PosicionAnclaje = PosicionBarco + lookAtanclaje;
+    }
+    float movimientoobj(float posicion, float prueba, float vel)
+    {
+        posicion = posicion + (h * vel * prueba);
+        
+        return posicion;
+    }
+    float velocidadobj(float fuerza, float velo)
+    {
+
+        velo += fuerza * h;
+        return velo;
+    }
+    //void curvaparametrica(float [] Pos)
+    //{
+    //    Matrix4x4 (1, Pos[0], Mathf.Pow(Pos[0], 2), Mathf.Pow(Pos[0], 3), Mathf.Pow(Pos[1], 2), Mathf.Pow(Pos[1], 3), 1, Pos[2], Mathf.Pow(Pos[2], 2), Mathf.Pow(Pos[2], 3), 1, Pos[3], Mathf.Pow(Pos[3], 2), Mathf.Pow(Pos[3], 3));
+    //   float[,] arrayx = new float[4, 4] { { 1, Pos[0],  Mathf.Pow(Pos[0],2), Mathf.Pow(Pos[0],3) },
+    //                            { 1, Pos[1],  Mathf.Pow(Pos[1],2), Mathf.Pow(Pos[1],3) },{ 1, Pos[2],  Mathf.Pow(Pos[2],2), Mathf.Pow(Pos[2],3) },{ 1, Pos[3],  Mathf.Pow(Pos[3],2), Mathf.Pow(Pos[3],3) } };
+
+    //    float[] arrayy= new float[4] {0.0f,5.0f,-5.0f,0.0f };
+    //    float[] arrayinverse = new float[4];
+    //}
+
+    void movolas()
+    {
+        //Movimiento en y
+        if (y <= 5 && prueba3 == true)
+        {
+            y = y + aumento;
+
+        }
+        else
+        {
+            prueba3 = false;
+            if (y >= -5)
+            {
+                y = y - aumento;
+            }
+            else
+            {
+                prueba3 = true;
+            }
+
+        }
+        //Movimiento rotatorio
+        if (alpha < 0.5236f && prueba2 == true && prueba4==true)
+        {
+            alpha = alpha + aumentotheta;
+
+        }
+        else if(prueba4==true)
+        {
+            prueba2 = false;
+            if (alpha >= 0.0f && pruebasopotosientasmil == false && prueba4 == true)
+            {
+              
+                alpha = alpha - aumentotheta;
+               
+               
+            }
+            else
+            {
+                alpha = 6.2832f;
+                pruebasopotosientasmil = true;
+                prueba4 = false;
+            }
+        }
+        if (alpha >= 5.76f && pruebasopotosientasmil == true && prueba4==false)
+        {
+            print("Otro: " + prueba4);
+            print("entreee");
+            alpha = alpha - aumentotheta;
+        }
+        else if(prueba4 == false)
+        {
+            pruebasopotosientasmil = false;
+            if (alpha <= 6.2832f)
+            {
+                alpha = alpha + aumentotheta;
+            }
+            else
+            {
+                alpha = 0.0f;
+                prueba2 = true;
+                prueba4=true;
+            }
+
+        }
+        print("Alpha: "+alpha);
+        barquito.transform.Rotate(alpha, 0f, 0f);
     }
 }
